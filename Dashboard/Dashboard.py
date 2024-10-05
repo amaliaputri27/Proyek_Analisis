ec2dff3b1ea933ad7302c7bbb23ee4317df43277
amaliaputri27 <aleaaap9@gmail.com>
2024-10-06 01:52:03 +0700

    Update Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -19,8 +19,3 @@ df_day = pd.read_csv('Data/day.csv')
 # Random sampling of all rows
 df_day_sample = df_day.sample(frac=1)
 
-# Descriptive statistics for DAY
-st.subheader('Descriptive Statistics for day Data')
-st.write("Statistika deskriptif dilakukan untuk memahami karakteristik data, termasuk ukuran pusat, penyebaran, dan pola.")
-day_stats = df_day_sample.describe(include='all')
-st.write(day_stats)
