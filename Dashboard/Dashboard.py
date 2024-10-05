30a882bb0a1c5e1fd112722674b1d53959231b14
amaliaputri27 <aleaaap9@gmail.com>
2024-10-06 03:29:49 +0700

    Update Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -318,6 +318,9 @@ st.write(f"Rata-rata jumlah sewa pada hari kerja dengan cuaca buruk: {average_cn
 st.write(f"Jumlah sewa maksimum: {max_cnt}")
 st.write(f"Jumlah sewa minimum: {min_cnt}")
 
+# kesimpulan
+st.write("Grafik di atas menunjukkan fluktuasi jumlah penyewaan sepeda pada hari kerja dengan cuaca buruk, dengan tren penyewaan berkisar antara 100 hingga 200 penyewaan per hari, namun terdapat lonjakan signifikan yang mencapai lebih dari 700 penyewaan, yang kemungkinan dipengaruhi oleh faktor-faktor seperti acara khusus atau kebutuhan mendesak. Meskipun cuaca buruk umumnya berdampak negatif pada penggunaan sepeda, beberapa orang tetap memilih untuk menyewa sepeda. Saya mengkaji ulang pertanyan, dan mendapatkan beberapa ide ataupun strategi baru, seperti memberikan diskon khusus pada hari cuaca buruk, kampanye promosi berbasis cuaca melalui email atau aplikasi, serta pengembangan fitur yang memberikan rekomendasi aktivitas saat cuaca tidak mendukung. Selain itu, peluang kolaborasi dengan bisnis lokal dan fokus pada iklan yang menyoroti manfaat bersepeda sebagai solusi untuk tetap aktif meski cuaca buruk juga dapat dioptimalkan.")
+
 # Menjawab pertanyaan 2
 st.write("Berdasarkan analisis ini, saya memberikan beberapa strategi marketing yang dapat diterapkan untuk meningkatkan jumlah pengguna pada hari kerja ketika cuaca buruk dapat mencakup:")
 
