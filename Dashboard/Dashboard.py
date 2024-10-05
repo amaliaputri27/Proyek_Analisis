29b2a0d56167e07c1d3ae7b609fe807ff315fc62
amaliaputri27 <aleaaap9@gmail.com>
2024-10-05 20:01:34 +0700

    Update Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -11,4 +11,27 @@ st.write("Email: m384b4kx0446@bangkit.academy")
 st.write("ID Dicoding: aleailearn")
 
 # Memuat Data
+st.write("Gathering Data")
 data = pd.read_csv('Bangkit7/Data/day.csv')
+
+# Memuat data dari kedua CSV
+df_day = pd.read_csv('data/day.csv')
+df_hour = pd.read_csv('data/hour.csv')
+
+# Menggabungkan data berdasarkan kolom 'dteday'
+df_merged = pd.merge(df_day, df_hour, on='dteday')
+
+# Menampilkan beberapa baris pertama data yang digabungkan
+print(df_merged.head())
+
+st.write("Assesing Data")
+# Menilai data df_day
+
+df_day.info()
+
+# Mengonversi kolom 'dteday' ke format datetime
+df_day['dteday'] = pd.to_datetime(df_day['dteday'])
+
+# Memeriksa tipe data setelah konversi
+print(df_day.dtypes)
+
